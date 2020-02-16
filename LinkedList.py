02eb04df9b54559c730842bfa5e0ea5c621cb148
SARAN SURYA RAVICHANDRAN <saransurya199@gmail.com>
2020-02-16 19:02:21 +0530

    update_2

--- LinkedList.py
+++ LinkedList.py
@@ -1,53 +1,42 @@
-class node(object):
-    def __init__(self,data):      # Node Constructor
+class node:
+    def __init__(self,data):             #main node constructor
         self.data=data
-        self.next=None
-
-    def __str__(self):           #stringifying constructor (returns a data of head or tail)
+        self.next=None 
+    def __str__(self):                  # string constructor for result
         return str(self.data)
-
 class linked(node):
-    def __init__(self):          # constructor for head and tail
-        self.head=None 
+    def __init__(self):
+        self.head=None                  #head and tail consrtructor
         self.tail=None 
-
-    def insert(self,data):                      #function to insert values O(1)
-        if(self.head==None):
+    def add(self,data):
+        if(self.head==None):                         #initial check
             self.head=self.tail=node(data)
         else:
-            new=node(data)                                                    
-            if(self.tail.next==None):
-                self.tail.next=new
+            if(self.tail.next==None): 
+                self.tail.next=node(data)                  #function to insert timecom: O(1)
                 self.tail=self.tail.next
-            
-    '''            
-    def pr(self):
+
+    def __str__(self):
+        if(self.head==self.tail): return(self.head.data)
+        else:
+            c=self.head
+            x=""
+            while(c.next):
+                x+=str(c)
+                c=c.next                                            #accesed directly using print() in driver program
+            x+=str(c)
+            return(x)
+    def print(self):
         ans=[]
         c=self.head
         while(c.next):
-            ans.append(c.data)                         #alternate function to print(values)  O(n)
+            ans.append(c.data)                               #accesed using print(self.print) in driver program
             c=c.next
         ans.append(c.data)
         return ans
-    '''
+a=list(map(int,input().split()))
+b=linked()
+for i in a: b.add(i)
+print(b)                                                     # direct access of the obkecct of the class
+print("list output : ",*b.print(),sep='')                   #accessing using self.print()
 
-
-    def __str__(self):
-        if(self.head==self.tail):
-            return "[" + str(self.head) + "]"
-        else:
-            c=""
-            x=self.head
-            while(x.next):
-                c+=str(x)
-                c+=","
-                x=x.next
-            c+=str(x)
-            return "["+c+"]"
-
-#Driver Function
-
-a=linked()
-t=list(map(int,input().split()))   #input for a list
-for i in t: a.insert(i)
-print(a)                           #printing the list using the constructor __str__ in linked() 
