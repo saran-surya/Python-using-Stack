bb087e47506211fdb6dc7bf99c039565ce114d5a
SARAN SURYA RAVICHANDRAN <saransurya199@gmail.com>
2020-02-16 16:14:25 +0530

    added linked list

---
+++ LinkedList.py
@@ -0,0 +1,51 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+    def __str__(self):
+        return str(self.data)
+
+class linked(node):
+    def __init__(self):
+        self.head=None 
+        self.tail=None 
+
+    def insert(self,data):
+        if(self.head==None):
+            self.head=self.tail=node(data)
+        else:
+            new=node(data)
+            if(self.tail.next==None):
+                self.head.next=new
+                self.tail.next=new
+                self.tail=self.tail.next
+                
+    def pr(self):
+        ans=[]
+        c=self.head
+        while(c.next):
+            ans.append(c.data)
+            c=c.next
+        ans.append(c.data)
+        return ans
+
+
+
+    def __str__(self):
+        if(self.head==self.tail):
+            return "[" + str(self.head) + "]"
+        else:
+            c=""
+            x=self.head
+            while(x.next):
+                c+=str(x)
+                c+=" "
+                x=x.next
+            c+=str(x)
+            return "["+c+"]"
+
+a=linked()
+a.insert(1)
+a.insert(2)
+print(a)
--- post_to_prefix.py
+++ post_to_prefix.py
@@ -1,6 +1,6 @@
 a=list(input())
 opr,ans=["+","-","/","*","^"],[]
-for i in a:
+for i in a[::-1]:
     if i not in opr:ans.append(i)
-    else:ans.append(i+ans.pop(-2)+ans.pop())
+    else:ans.append(ans.pop()+ans.pop()+i)
 print(*ans)
